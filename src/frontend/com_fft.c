68fd8b8fa1dfc3a2d87ff58d1dba550c51b157dd
Holger Vogt <holger.vogt@uni-due.de>
2026-02-18 14:54:26 +0100

    PMOS and ring oscillator degradation examples

--- src/frontend/com_fft.c
+++ src/frontend/com_fft.c
@@ -60,6 +60,9 @@ com_fft(wordlist *wl)
     length = (plot_cur->pl_scale)->v_length;
     time = (plot_cur->pl_scale)->v_realdata;
     span = time[length-1] - time[0];
+    if (length > 1) {
+        span += time[1] - time[0];
+    }
 
 #ifdef HAVE_LIBFFTW3
     fpts = length/2 + 1;
